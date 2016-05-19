3c79f70cc293dd2109eb3f69bd3c48fecf5fd56d
Remco de Hoon <rajhoon@avans.nl>
2016-05-19 10:15:44 +0200

    V1 Maven structure

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import presentation.Controller;
+
+public class Main {
+
+// Maakt het hoofdscherm zichtbaar bij opstarten en maakt Manager aan
+    public static void main(String[] args) {
+        Controller c = new Controller();
+        c.makeVisible("Mainmenu");
+    }
+}
